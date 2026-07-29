ab9889b05ef6a22be4e1ec4fde9f6536ac5762ea
aamerkotbicode <aamerkotbi6924@gmail.com>
2026-07-29 11:59:15 +0300

    Add hero class with constructor and introduce method

--- Q1_HERO.php
+++ Q1_HERO.php
@@ -1,1 +1,21 @@
 <?php
+class hero {
+public $name;
+public $Power;
+function __construct($name, $Power) {
+    $this->name = $name;
+    $this->Power = $Power;
+}
+function Introduce() {
+echo "\nThe Hero's Name: " . $this->name . "\nAnd his Superpower is: " . $this->Power ."\n\n";
+}
+}
+
+$hero1 = new hero("Ja'far", "Flying");
+$hero1->Introduce();
+
+$hero2 = new hero("Zayd", "Faith");
+$hero2->Introduce();
+
+$hero3 = new hero("Abdullah", "Poetry");
+$hero3->Introduce();
