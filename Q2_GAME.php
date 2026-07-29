ec19abdea87723a9fd781159cc9e14a1732da772
aamerkotbicode <aamerkotbi6924@gmail.com>
2026-07-29 12:21:34 +0300

    Add game class with properties and methods for discount and info display

--- Q2_GAME.php
+++ Q2_GAME.php
@@ -1,1 +1,29 @@
 <?php
+
+class game {
+    public $name;
+    public $price;
+
+
+
+public function __construct($name, $price) {
+    $this->name = $name;
+    $this->price = $price;
+}
+
+public function ApplyDiscount() {
+    $newprice = $this->price - ($this->price * 0.25);
+    $this->price = $newprice;
+}
+
+public function GetGameinfo() {
+    echo "\nGame Name: ". $this->name ."\nGame Price: ". $this->price . "\n";
+}
+}
+$game1 = new game("Minecraft", 29.9);
+$game1->ApplyDiscount();
+$game1->GetGameinfo();
+
+$game2 = new game("Mario", 79.99);
+$game2->GetGameinfo();
+
